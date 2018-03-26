f67f5680c7e73b860d254fc3f9ad3ad0ebffffcb
Turák Bence <bence.turak@gmail.com>
2018-03-26 21:33:55 +0200

    TotalStationClient class init commit

---
+++ lib/totalstationclient.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('../ulyxes/pyapi/')
+from leicatps1200 import LeicaTPS1200
+from totalstation import TotalStation
+from stationcommands import StationCommands
+import logging
+import socket
+import json
+
+
+
+class TotalStationClient(TotalStation):
+
+    def Trial(self, param1):
+
+        msg = self.measureUnit.TrialMsg(param1)
+        return self._process(msg)
